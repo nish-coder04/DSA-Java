d166dd88133167d6a964d3fb8c42b9ad11e16a4a
Nishtha Shukla <your-email@example.com>
2026-07-01 16:24:16 +0530

    Day 5 - Kadane's Algorithm

--- Arrays/Arrays5.java
+++ Arrays/Arrays5.java
@@ -1,6 +1,6 @@
 
 //Missing Elements from Array of Duplicates
-import java.util.HashSet;
+/*import java.util.HashSet;
 
 public class Arrays5 {
     static int findMissing(int[] arr, int n) {
@@ -30,4 +30,52 @@ public class Arrays5 {
         }
     }
 
+}*/
+
+//Maximum Subarray! Kadane's Algorithm 
+/*public class Arrays5 {
+    static int maxSubArray(int[] nums) {
+        int sum = 0;
+        int sumSubArray = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sumSubArray = nums[i]; 
+            if (sumSubArray > sum)
+                sum = sumSubArray;
+            for (int j = i + 1; j < nums.length; j++) {
+                sumSubArray += nums[j]; 
+                if (sumSubArray > sum)
+                    sum = sumSubArray;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        int maxSum = maxSubArray(nums);
+        System.out.println("Maximum subarray sum is: " + maxSum);
+    }
+}*/
+
+public class Arrays5 {
+    static int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum + nums[i];
+            if (sum > maxSum) {
+                maxSum = sum;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        int result = maxSubArray(nums);
+        System.out.println("Maximum subarray sum is: " + result);
+    }
 }
