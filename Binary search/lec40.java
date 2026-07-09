1e54fd6b8bf34ed4d75972b7225b0105db8aff59
nish-coder04 <nishthas615@gmail.com>
2026-07-09 11:34:43 +0530

    Day 11 - Sqrt and precise Sqrt usking binary search

--- Binary search/lec40.java
+++ Binary search/lec40.java
@@ -1,5 +1,5 @@
 //Sqrt(x)
-public class lec40 {
+/*public class lec40 {
     static int Sqrt(int x) {
         int s = 1;
         int e = x;
@@ -23,12 +23,58 @@ public class lec40 {
         }
         return answer;
     }
-
     public static void main(String[] args) {
         int x = 56;
         int squareRoot = Sqrt(x);
         System.out.println(squareRoot);
     }
-}
+}*/
 
-//
+//Sqrt(x) with precision
+public class lec40 {
+    static double Sqrt(int x) {
+        int s = 1;
+        int e = x;
+        double answer = -1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (x == 0) {
+                answer = 0;
+                return answer;
+            }
+            if (mid == x / mid) {
+                answer = mid;
+                return answer;
+            }
+            if (mid > x / mid) {
+                e = mid - 1;
+            } else {
+                answer = mid;
+                s = mid + 1;
+            }
+        }
+        double factor = 1;
+        int totalPrecision = 3;
+        for (int round = 1; round <= totalPrecision; round++) {
+            factor = factor / 10;
+            for (int i = 1; i <= 10; i++) {
+                double newAns = answer + factor;
+                if (newAns * newAns == x) {
+                    answer = newAns;
+                    return answer;
+                } else if (newAns * newAns < x) {
+                    answer = newAns;
+                } else {
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int x = 56;
+        double SquareRoot = Sqrt(x);
+        System.out.println(SquareRoot);
+    }
+}
